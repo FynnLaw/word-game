1ec84237a8e900da6301a28fc867601cbc8aacf3
FynnLaw <fynnlaw@gmail.com>
2016-07-25 15:49:54 +0800

    前端大致完毕

--- src/main/webapp/admin/js/wordGame.js
+++ src/main/webapp/admin/js/wordGame.js
@@ -1,118 +1,261 @@
     Ext.QuickTips.init();
     var _cob;
    	Ext.onReady(function(){
-   		Ext.override(Ext.menu.DateMenu, {  
-   		    render : function() {  
-   		        Ext.menu.DateMenu.superclass.render.call(this);  
-   		        if (Ext.isGecko || Ext.isSafari || Ext.isChrome) {  
-   		            this.picker.el.dom.childNodes[0].style.width = '178px';  
-   		            this.picker.el.dom.style.width = '178px';  
-   		        }  
-   		    }  
-   		}); 
    		
-   		var qrUrl = path + "/user!";
-   		var order;
-        store = new Ext.data.Store({
-			url : qrUrl+"listRole.action",
-			reader : new Ext.data.JsonReader({
-				root : 'products',
-				totalProperty : 'totalCount',
-				id : 'querydate',
-				fields : [
-					{name:  'roleName'},
-					{name : 'id'},
-				]
-			}),
-			remoteSort : true
-		});
-        
-		store.load({params:{start:0,limit:20}});
-        
-        var columnBreakList=new Ext.grid.ColumnModel(
-            [
-            	new Ext.grid.RowNumberer(),
-            	{header:"列表",align:'center',dataIndex:"roleName",sortable:true}, 
-	            {header:"操作",align:'center',dataIndex:"id",
-	            renderer: function (value, meta, record) {
-	            	
-	            			var formatStr = "<input id = 'bt_edit_" + record.get('id')
-								+ "' onclick=\"showEditRole('" + record.get('id') + "','"
-								+ record.get('roleName') 
-								+ "');\" type='button' value='编辑' width ='15px'/>&nbsp;&nbsp;"; 
+	var qrUrl = path + "/user!";
+	var order;
+    store = new Ext.data.Store({
+		url : qrUrl+"listRole.action",
+		reader : new Ext.data.JsonReader({
+			root : 'products',
+			totalProperty : 'totalCount',
+			id : 'querydate',
+			fields : [
+				{name:  'roleName'},
+				{name : 'id'},
+			]
+		}),
+		remoteSort : true
+	});
+    
+	store.load({params:{start:0,limit:20}});
+    
+    var columnBreakList=new Ext.grid.ColumnModel(
+        [
+        	new Ext.grid.RowNumberer(),
+        	{header:"游戏列表",align:'center',dataIndex:"roleName",sortable:true}, 
+            {header:"操作",align:'center',dataIndex:"id",
+            renderer: function (value, meta, record) {
+            	
+            			var formatStr = "<input id = 'bt_edit_" + record.get('id')
+							+ "' onclick=\"showEditRole('" + record.get('id') + "','"
+							+ record.get('roleName') 
+							+ "');\" type='button' value='编辑' width ='15px'/>&nbsp;&nbsp;"; 
 
-	            			var deleteBtn = "<input id = 'bt_delete_" + record.get('id')
-								+ "' onclick=\"deleteRole('" + record.get('id')
-								+ "');\" type='button' value='删除' width ='15px'/>&nbsp;&nbsp;";
-										            			
-            				var resultStr = String.format(formatStr);
-            				return "<div>" + resultStr+deleteBtn + "</div>";
-        				  }.createDelegate(this)
-	            },
-            ] 
-        ); 
-        
-        
-        var breakListGridPanel = new Ext.grid.EditorGridPanel({ 
-			region:'center',
-			border:false,
-			viewConfig: {
-	            forceFit: true, //让grid的列自动填满grid的整个宽度，不用一列一列的设定宽度。
-	            emptyText: '系统中还没有任务'
-	        },
-            cm:columnBreakList, 
-            store:store, 
-            autoExpandColumn:0, 
-            loadMask:true, 
-            frame:true, 
-            autoScroll:true, 
-        });
-        
-        var addButton = new Ext.Button({
-        	region:'south',
-        	text : "新增文字游戏",
-        	height:40,
-        	iconCls : "addButton",　　
-        	id:"addButton", 
-        	handler : function() {
-        		alert(1);
-        	}
-        });
-        
-    var breakListPanel = new Ext.Panel({
+            			var deleteBtn = "<input id = 'bt_delete_" + record.get('id')
+							+ "' onclick=\"deleteRole('" + record.get('id')
+							+ "');\" type='button' value='删除' width ='15px'/>&nbsp;&nbsp;";
+									            			
+        				var resultStr = String.format(formatStr);
+        				return "<div>" + resultStr+deleteBtn + "</div>";
+    				  }.createDelegate(this)
+            },
+        ] 
+    ); 
+    
+    var breakListGridPanel = new Ext.grid.EditorGridPanel({ 
+		region:'center',
+		viewConfig: {
+            forceFit: true // 让grid的列自动填满grid的整个宽度，不用一列一列的设定宽度。
+        },
+        cm:columnBreakList, 
+        store:store, 
+    });
+    
+    var addButton = new Ext.Button({
+    	region:'south',
+    	text : "新增文字游戏",
+    	height:40,
+    	iconCls : "addButton",　　
+    	id:"addButton", 
+    	handler : function() {
+    		alert(1);
+    	}
+    });
+    
+    var listPanel = new Ext.Panel({
   		region:'west',
-  		title:"文字闯关列表",  
+  		title:'文字闯关列表',  
 		layout:'border',
 		border:false,
 		width:320,
 		items:[breakListGridPanel,
 		       addButton]
 	});
+    
+    var formPanel =  new Ext.form.FormPanel({
+    	border:false,
+        items: [
+           {xtype:"field", width:180,id: "oldpwd", fieldLabel: "名称", inputType: "password"},
+           {xtype:"displayfield", width:180,id: "oldpwdes2t", fieldLabel: "ID(KEY)"},
+           {xtype:"tbtext",id: "oldpwdesdt", text : "预览",style:"font-size:medium;font-weight:bold;text-align: center;"}
+        ]
+     });
+    
+    var northPanel = new Ext.Panel({
+  		region:'north',
+  		width:'100%',
+        border:false,
+		items:[formPanel]
+	});
         
   	var wordGameTreePanel = new Ext.tree.TreePanel({
-         region:'center',
-         title:"文字闯关编辑器",
-         layout: 'accordion',
+  		 region:'center',
          border:false,
          loader: new Ext.tree.TreeLoader({
 			        url: path+'/user!getTree.action',
 			        requestMethod: 'GET'
 		    		}),
-		    //构造根节点
+		    // 构造根节点
 			root: new Ext.tree.AsyncTreeNode({
 			    id: 'root',
 			    text: '数据管理',
 				expanded: true
-			})
+			}),
+			buttons: [{text: '保存' ,width:70,height:20,handler:function (){
+				
+			}},
+			          {text: '清空全部' ,width:70,height:20,handler:function (){
+				          gridForm.getForm().submit({
+				        	  url : 'bookinput.do?actionsign=input', 
+				        	  method : 'post'
+				        	  });
+				          }
+			}],
+			buttonAlign : 'center'
 		});
+  	
+  	var wordGamePanelWestPanel = new Ext.Panel({
+        border:false,
+  		style: 'margin:20px',
+		items:[northPanel,
+		       wordGameTreePanel]
+	});
+  	
+  	var wordGamePanelWest = new Ext.Panel({
+  		region:'west',
+        border:false,
+        width :'30%',
+        heigth:'100%',
+		items:[wordGamePanelWestPanel]
+	});
+  	
+  	 var formPanelEast =  new Ext.form.FormPanel({
+     	 border:false,
+         items: [
+                 {xtype:"tbtext", width:360,id: "1", text: "层级内容编辑",style:"font-size:medium;font-weight:bold;text-align: center;"},
+                 {xtype:"displayfield", width:180,id: "2", fieldLabel: "层级编号"},
+                 {xtype:"textarea", width:180,id: "3", fieldLabel: "文字内容", inputType: "text"},
+	             {xtype:"combo", 
+	              width:180,
+	              id: "combo0", 
+	              fieldLabel: "选项0",  
+	              store:new Ext.data.SimpleStore({
+	                 fields:['value','text'],
+	                 data:[
+	                 ['value1','text1'],
+	                 ['value2','text2'],
+	                ]
+	              }),
+	              displayField:'text',
+	              valueField:'value',
+	              mode:'local',
+	              emptyText:'请选择',
+	              readOnly:false},
+	              {xtype:"combo", 
+	            	  width:180,
+	            	  id: "combo1", 
+	            	  fieldLabel: "选项1",  
+	            	  store:new Ext.data.SimpleStore({
+	            		  fields:['value','text'],
+	            		  data:[
+	            		        ['value1','text1'],
+	            		        ['value2','text2'],
+	            		        ]
+	            	  }),
+	            	  displayField:'text',
+	            	  valueField:'value',
+	            	  mode:'local',
+	            	  emptyText:'请选择',
+	            	  readOnly:false},
+            	  {xtype:"combo", 
+            		  width:180,
+            		  id: "combo2", 
+            		  fieldLabel: "选项2",  
+            		  store:new Ext.data.SimpleStore({
+            			  fields:['value','text'],
+            			  data:[
+            			        ['value1','text1'],
+            			        ['value2','text2'],
+            			        ]
+            		  }),
+            		  displayField:'text',
+            		  valueField:'value',
+            		  mode:'local',
+            		  emptyText:'请选择',
+            		  readOnly:false},
+        		  {xtype:"combo", 
+        			  width:180,
+        			  id: "combo3", 
+        			  fieldLabel: "选项3",  
+        			  store:new Ext.data.SimpleStore({
+        				  fields:['value','text'],
+        				  data:[
+        				        ['value1','text1'],
+        				        ['value2','text2'],
+        				        ]
+        			  }),
+        			  displayField:'text',
+        			  valueField:'value',
+        			  mode:'local',
+        			  emptyText:'请选择',
+        			  readOnly:false},
+    			  {xtype:"combo", 
+    				  width:180,
+    				  id: "combo4", 
+    				  fieldLabel: "选项4",  
+    				  store:new Ext.data.SimpleStore({
+    					  fields:['value','text'],
+    					  data:[
+    					        ['value1','text1'],
+    					        ['value2','text2'],
+    					        ]
+    				  }),
+    				  displayField:'text',
+    				  valueField:'value',
+    				  mode:'local',
+    				  emptyText:'请选择',
+    				  readOnly:false},
+			  {xtype:"textarea", width:180,id: "3dd", fieldLabel: "结束提示语", inputType: "text"}
+			  
+         ],
+         style: 'padding:20px',
+         buttons:[{xtype:"button",id: "3dd22",text : '保存',listeners:{
+			  click:function(){
+				  console.log(1);
+			  }
+		  	}
+		  },
+		  {xtype:"button",id: "3dd2112",text : '取消',listeners:{
+			  click:function(){
+				  console.log(1);
+			  }
+		  	}
+		  }],
+		  buttonAlign : 'center'
+      });
+  	
+  	var wordGamePanelEast = new Ext.Panel({
+  		region:'center',
+        items: [formPanelEast]
+	});
 	
+  	var wordGamePanel = new Ext.Panel({
+  		region:'center',
+  		title:'文字闯关编辑器',  
+		layout:'border',
+		border:false,
+		items:[wordGamePanelWest,
+		       wordGamePanelEast]
+	});
+  	
    var viewport=new Ext.Viewport({
        enableTabScroll:true,
        layout:'border',
        defaultType: 'Viewport',
        items:[
-           breakListPanel,
-           wordGameTreePanel
+           listPanel,
+           wordGamePanel
    	   ]
    });
    });
@@ -123,7 +266,7 @@
 		store.reload({
 			params: {start:0,limit:20},
 			callback: function(records, options, success){
-//				console.log(records);
+// console.log(records);
 			},
 			scope: store
 		});
@@ -131,7 +274,6 @@
 	
 	
 	function saveInfo(oldName,newName,_id){
-		//console.log(_id);
 		if(oldName != newName){
 			Ext.Msg.confirm('保存数据', '确认?',function (button,text){if(button == 'yes'){
 				Ext.Ajax.request( {
@@ -143,7 +285,7 @@
 					  },
 					  success : function(response, options) {
 					   var o = Ext.util.JSON.decode(response.responseText);
-					   //alert(o.i_type);
+					   // alert(o.i_type);
 					   if(o.i_type && "success"== o.i_type){
 					   	
 					   }else{
@@ -283,7 +425,7 @@
     			lines:true,
     			loader: new Ext.tree.TreeLoader({
     				url: path+'/user!getAuthTree.action?roleId='+roleId,
-//    			        url: '../admin/js/tree.txt',
+// url: '../admin/js/tree.txt',
     				requestMethod: 'GET'
     			}),
     			root: new Ext.tree.AsyncTreeNode({
@@ -292,7 +434,7 @@
     					expanded: true
     				})
     	 });
-    	//判断是否有子结点被选中
+    	// 判断是否有子结点被选中
     	var childHasChecked = function(node){
 			var childNodes = node.childNodes;
 			if(childNodes || childNodes.length>0){
@@ -383,11 +525,11 @@
     	
     	var authWin = new Ext.Window({
     		width : 300,
-//    		height:110,
+// height:110,
     		autoHeight : true,
     		title : '权限编辑',
     		defaults : {// 表示该窗口中所有子元素的特性
-//    			border : false
+// border : false
     			// 表示所有子元素都不要边框
     		},
     		plain : true,// 方角 默认
